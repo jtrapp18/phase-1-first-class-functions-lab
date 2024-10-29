be06c4792f2b6b2f56241f7b01170d45ad4cf94e
+ <jacqueline.trapp18@gmail.com>
2024-10-29 12:25:43 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = scuberDrivers => scuberDrivers.slice(0, 2)
+const returnLastTwoDrivers = scuberDrivers  => scuberDrivers.slice(scuberDrivers.length-2, scuberDrivers.length)
+const selectingDrivers  = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = multiplier => function(fare) {return multiplier*fare}
+const fareDoubler = fare => createFareMultiplier(2)(fare)
+const fareTripler = fare => createFareMultiplier(3)(fare)
+const selectDifferentDrivers = (drivers, cb) => cb(drivers)
